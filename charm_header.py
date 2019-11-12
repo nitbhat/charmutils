825740a53b240fccc40f1212e06046188afba810
Nitin Bhat <nbhat4@illinois.edu>
2019-11-12 11:35:04 -0800

    Add hpcadv entry

--- charm_header.py
+++ charm_header.py
@@ -14,15 +14,18 @@ elif(hostname_full.find('cori') != -1):
   key="cori"
 elif(hostname_full.find('golub') != -1):
   key="golub"
+elif(hostname_full.find('hpcadv') != -1):
+  key="hpcadv"
 else:
   key = 'iforge'
 
 buildType = 'prod'
+
 basedirs = {
     "iforge" : "/ui/cwi/nitin/software/charm",
     "cori" : "/global/homes/n/nbhat4/software/charm_2",
     "bridges" : "/pylon5/ac7k4vp/nbhat4/charm",
-    "hpcadv" : "/global/home/users/nitinb/charm_1",
+    "hpcadv" : "/global/home/users/nitinb/charm",
     "golub" : "/home/nbhat4/charm"
 }
 
@@ -116,7 +119,8 @@ smpSkipLinesModifierMap = {
 hyphen="-"
 slash="/"
 space=" "
-target="charm++"
+#target="charm++"
+target="ChaNGa"
 suffix="prod"
 #basearch = basebuild+"-linux-x86_64"
 options = " --with-production --enable-error-checking --suffix="+suffix
@@ -125,68 +129,67 @@ num_proc = " -j"+str(build_proc) + " "
 debug_opts=""
 
 
-
-#buildmodes = ["smp","nonsmp"]
-
-
-#basebuild="verbs"
-#basedir="/home/nbhat4/scratch/charm/" #golub
-
-#basedir="/ui/cwi/nitin/software/charm/"
-
-
-#outputbase="/home/nbhat4/scratch/results/zc_exp/" #golub
-#outputbase="/ui/cwi/nitin/software/charmutils/results/iforge/zc_exp/" #iforge
-#outputbase="/Users/nitinbhat/Work/software/charmutils/results/iforge/zc_exp/" #iforge
-
+##buildmodes = ["smp","nonsmp"]
+#
+#
+##basebuild="verbs"
+##basedir="/home/nbhat4/scratch/charm/" #golub
+#
+##basedir="/ui/cwi/nitin/software/charm/"
+#
+#
+##outputbase="/home/nbhat4/scratch/results/zc_exp/" #golub
+##outputbase="/ui/cwi/nitin/software/charmutils/results/iforge/zc_exp/" #iforge
+##outputbase="/Users/nitinbhat/Work/software/charmutils/results/iforge/zc_exp/" #iforge
+#
+##scriptbases=[
+##"#!/bin/bash\n\
+###PBS -l walltime=00:10:00\n\
+###PBS -l nodes=2:ppn=1\n\
+###PBS -N myjob\n\
+###PBS -j oe\n",
+##"#!/bin/bash\n\
+###PBS -l walltime=00:20:00\n\
+###PBS -l nodes=2:ppn=2\n\
+###PBS -N myjob\n\
+###PBS -j oe\n"]
+##
+#extraRun=""
+#
+#basedir="/pylon5/ac7k4vp/nbhat4/charm/"
+#outputbase="/pylon5/ac7k4vp/nbhat4/charmutils/results/bridges_ofi/zc_exp/"
+#
 #scriptbases=[
 #"#!/bin/bash\n\
-##PBS -l walltime=00:10:00\n\
-##PBS -l nodes=2:ppn=1\n\
-##PBS -N myjob\n\
-##PBS -j oe\n",
+##SBATCH -N 2\n\
+##SBATCH -p RM\n\
+##SBATCH -t 0:20:00\n\
+##SBATCH --ntasks-per-node 1\n",
+#
 #"#!/bin/bash\n\
-##PBS -l walltime=00:20:00\n\
-##PBS -l nodes=2:ppn=2\n\
-##PBS -N myjob\n\
-##PBS -j oe\n"]
+##SBATCH -N 2\n\
+##SBATCH -p RM\n\
+##SBATCH -t 0:30:00\n\
+##SBATCH --ntasks-per-node 1\n"]
 #
-extraRun=""
-
-basedir="/pylon5/ac7k4vp/nbhat4/charm/"
-outputbase="/pylon5/ac7k4vp/nbhat4/charmutils/results/bridges_ofi/zc_exp/"
-
-scriptbases=[
-"#!/bin/bash\n\
-#SBATCH -N 2\n\
-#SBATCH -p RM\n\
-#SBATCH -t 0:20:00\n\
-#SBATCH --ntasks-per-node 1\n",
-
-"#!/bin/bash\n\
-#SBATCH -N 2\n\
-#SBATCH -p RM\n\
-#SBATCH -t 0:30:00\n\
-#SBATCH --ntasks-per-node 1\n"]
-
+##
 #
-
-
-postdir="/examples/charm++/zerocopy/direct_api/"
-
-reg_modes=["reg","prereg","unreg"]
-example="pingpong"
-run_proc = 2
-expname="directvsregrdma"
-
-filecontents=""
-
-args=["32 33554432 1000 100","32 33554432 1000 100 ++ppn 1 +pemap 0 +commap 1"]
-
-#Util Methods
-
-def getOutputFile(num_nodes, archopt_str, smp_index, basebuild, extraSuffix):
-  outputDir = charmutilsdirs[key] + slash + "results/" + key + slash + "bcast/";
-  outputFile   = outputDir + "reg_bcast_test_" + str(num_nodes) + "_" + basebuild + ("_" if extraSuffix != "" else "") + extraSuffix + "_" +  archopts[smp_index]
-  return outputFile
-
+#
+#postdir="/examples/charm++/zerocopy/direct_api/"
+#
+#reg_modes=["reg","prereg","unreg"]
+#example="pingpong"
+#run_proc = 2
+#expname="directvsregrdma"
+#
+#filecontents=""
+#
+#args=["32 33554432 1000 100","32 33554432 1000 100 ++ppn 1 +pemap 0 +commap 1"]
+#
+##Util Methods
+#
+#def getOutputFile(num_nodes, archopt_str, smp_index, basebuild, extraSuffix):
+#  outputDir = charmutilsdirs[key] + slash + "results/" + key + slash + "bcast/";
+#  outputFile   = outputDir + "reg_bcast_test_" + str(num_nodes) + "_" + basebuild + ("_" if extraSuffix != "" else "") + extraSuffix + "_" +  archopts[smp_index]
+#  return outputFile
+#
