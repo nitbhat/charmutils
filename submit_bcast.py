38e9c366e81e64126dc918a698136b508cf43c92
Nitin Bhat <nbhat4@illinois.edu>
2021-06-12 18:31:19 -0700

    Add submit scripts for bcast and ro
    Change-Id: Iaa13babf3300a8f033518a09edce2fc61ae302d2

---
+++ submit_bcast.py
@@ -0,0 +1,28 @@
+from charm_header import *
+num_nodes=2
+max_nodes=8
+ppn = ppnmap[key]
+proc_per_node=proc_per_node_map[key]
+
+print "key is " + key
+
+print " basedir is " + basedirs[key]
+
+
+
+
+while num_nodes <= max_nodes:
+  smp_index=0
+  for archopt_str in archopts_str:
+    scriptname = "reg_bcast_test_" + str(num_nodes) + "_" + archopts[smp_index];
+    outputDir = charmutilsdirs[key] + slash + "results/" + key + slash + "bcast/";
+
+    for basebuild in basebuilds[key]:
+      scriptDir = charmutilsdirs[key] + slash + "scripts/" + key + slash + "bcast/";
+
+      if(smp_index == 1):
+        print "sbatch "+scriptDir + scriptname
+        os.system("sbatch "+scriptDir + scriptname)
+
+    smp_index = smp_index + 1;
+  num_nodes = num_nodes*2
